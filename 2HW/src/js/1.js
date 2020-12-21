21e307fda6a116c2f21e84a8814cad2ecacfc6d7
620474 <jklooop@gmail.com>
2020-12-21 18:06:21 +0300

    HW2
    HW2

---
+++ 2HW/src/js/1.js
@@ -0,0 +1,23 @@
+let app = Vue.createApp({
+    data() {
+        return {
+            sitename: 'Unofficial GOG-shop',
+            product: {
+                title: "Cyberpunk 2077",
+                description:
+                    "Cyberpunk 2077 приключенческая ролевая игра, Бдействие которой происходит в мегаполисе Найт-Сити, где власть, роскошь и модификации тела ценятся выше всего. Вы играете за V, наёмника в поисках уникального устройства, позволяющего обрести бессмертие. Вы сможете менять киберимпланты, навыки и стиль игры своего персонажа, исследуя огромный открытый мир, где ваши поступки влияют на ход сюжета и всё, что вас окружает.",
+                company: 'CD PROJEKT RED Studio',
+                publishing: 'CD PROJEKT RED Studio',
+                year: 2020,
+                price: 1999,
+                image: './src/1.jpg',
+
+            },
+            visible: true,
+            menu:"trailer",
+
+        }
+    },
+
+});
+let vm = app.mount("#app");
